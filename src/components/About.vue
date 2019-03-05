d3802c4632064ef48223dd7c35a42ed46f032ad4
Michael Baart <michael.baart96@gmail.com>
2019-03-05 09:12:53 -0800

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>
+      About
+    </h1>
+    <p> {{ msg }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
