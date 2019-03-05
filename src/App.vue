e5fccdb5952f46a484d6a446db981355217e22d1
Michael Baart <michael.baart96@gmail.com>
2019-03-05 12:31:35 -0800

    Add group member info

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <About msg="Welcome to Your Vue.js App"/>
+    <About msg="This is our project blah, blah, blah..."/>
+    <Me name="doge" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
   </div>
 </template>
 
 <script>
+// import Vue from 'vue'
 import About from './components/About.vue'
+import Me from './components/Me.vue'
 
 export default {
   name: 'app',
   components: {
-    About
-  }
+    About,
+    Me
+  },
 }
 </script>
 
---
+++ src/components/Me.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="me">
+    <h1> Who we are. </h1>
+    <img class="picture" :src="require('../assets/' + name + '.jpg')"/>
+    <div class="descripton"> 
+      <h1> {{ name }} </h1>
+      <p> {{ description }} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+// import Vue from 'vue'
+export default {
+  name: 'Me',
+  props: {
+    name: String,
+    description: String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+    #me{
+        width: 100%;
+        /* margin: 10%; */
+    }
+
+    .picture {
+        float: left;
+        width: 40%;
+    }
+
+    .description {
+        float: left;
+        width: 60%;
+    }
+</style>
