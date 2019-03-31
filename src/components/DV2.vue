7ad16d200cc20882e57515671212c973bd838855
sam charles <c.sam.charles@gmail.com>
2019-03-31 15:37:48 -0700

    create front end shell

---
+++ src/components/DV2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <svg ref="dv" width="500" height="300"></svg>
+    <p> {{msg}} </p>
+  </div>
+</template>
+
+<script>
+const d3 = require("d3");
+export default {
+  name: "DV2",
+  props: {
+    msg: String
+  },
+
+  mounted: function() {
+    d3.select(this.$refs.dv)
+      .append("circle")
+      .attr("cx", "250")
+      .attr("cy", "150")
+      .attr("r", "100");
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
---
+++ src/components/DV3.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <svg ref="dv" width="500" height="300"></svg>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+const d3 = require("d3");
+export default {
+  name: "DV3",
+  props: {
+    msg: String
+  },
+
+  mounted: function() {
+    // this.#el - is the root element in <template>
+    // in this case it is <svg> tag
+    d3.select(this.$refs.dv)
+      .append("circle")
+      .attr("cx", "250")
+      .attr("cy", "150")
+      .attr("r", "100");
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
